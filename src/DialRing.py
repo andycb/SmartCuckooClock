Fix issue where dial light would get stuck on. Correctly report the state of rhe dial, pendulum, light level, and and timer. Report the raw light level state. Improvements to light level calculation

src/DialRing.py
@@ -103,7 +103,7 @@ class DialRing:
         self.np.fill((0,0,0))
         self.np.write()
         self._colourOverride = None
-
+        self._pattern = None
 
     def set_dial_ring(self, colour: Colour, pattern: RingPatterns.BasePattern) -> None:
         if colour.red == 0 and colour.green == 0 and colour.blue == 0: